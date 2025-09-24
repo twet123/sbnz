ce60049d995c55061d0b5c4b0842ea38dfcecfab
Vladimir Popov <vladapopov02@gmail.com>
2025-09-24 21:56:34 +0200

    add CpuOverheatEvent

---
+++ model/src/main/java/com/ftn/sbnz/model/events/CpuOverheatEvent.java
@@ -0,0 +1,13 @@
+package com.ftn.sbnz.model.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.kie.api.definition.type.Expires;
+import org.kie.api.definition.type.Role;
+
+@Role(Role.Type.EVENT)
+@Expires("30m")
+@Data
+@AllArgsConstructor
+public class CpuOverheatEvent {
+}
