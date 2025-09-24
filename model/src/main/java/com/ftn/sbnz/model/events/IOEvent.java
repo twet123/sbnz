e7d43808e6d28deee3734efb03fb0811353313d5
Vladimir Popov <vladapopov02@gmail.com>
2025-09-24 21:57:14 +0200

    IOEvent add expiration

--- model/src/main/java/com/ftn/sbnz/model/events/IOEvent.java
+++ model/src/main/java/com/ftn/sbnz/model/events/IOEvent.java
@@ -1,10 +1,12 @@
 package com.ftn.sbnz.model.events;
 
 import lombok.Data;
+import org.kie.api.definition.type.Expires;
 import org.kie.api.definition.type.Role;
 
 @Role(Role.Type.EVENT)
 @Data
+@Expires("5m")
 public class IOEvent {
 
     private long processId;
