2c5e37cdc795610a4ad702ba4979073bcb9301d1
Vladimir Popov <vladapopov02@gmail.com>
2025-09-25 22:03:38 +0200

    append systemState model

--- model/src/main/java/com/ftn/sbnz/model/models/SystemState.java
+++ model/src/main/java/com/ftn/sbnz/model/models/SystemState.java
@@ -9,5 +9,13 @@ public class SystemState {
 
     private int availableMemory;
     private int totalMemory;
+    private int criticalMemoryLimit;
     private boolean cpuEnabled;
+
+    public SystemState(int availableMemory, int totalMemory, boolean cpuEnabled) {
+        this.availableMemory = availableMemory;
+        this.totalMemory = totalMemory;
+        this.criticalMemoryLimit = 0;
+        this.cpuEnabled = cpuEnabled;
+    }
 }
